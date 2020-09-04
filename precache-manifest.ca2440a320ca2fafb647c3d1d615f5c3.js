518f46dd125e9318801109678808fa6a946a8b8a
john londoño <soupnumber91@gmail.com>
2020-09-03 19:25:55 -0500

    Updates

---
+++ precache-manifest.ca2440a320ca2fafb647c3d1d615f5c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c48020e85e17e4cbb41bd30576a54e3",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "175de10cfdc50743207d",
+    "url": "/pokedex/static/js/2.beaa5f13.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.beaa5f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad4cb5d771a53b76e8f",
+    "url": "/pokedex/static/js/main.8dcc7a61.chunk.js"
+  },
+  {
+    "revision": "895f9ac5d2715e3448e3",
+    "url": "/pokedex/static/js/runtime-main.a57880e6.js"
+  }
+]);
